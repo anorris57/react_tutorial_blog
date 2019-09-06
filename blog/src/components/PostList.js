85f98de25e12a3f2c0a1241d98e6662573ca924e
Angela Norris <cocoanorris@yahoo.com>
2019-09-06 15:34:02 -0400

    creating postlist component

---
+++ blog/src/components/PostList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+//use life cycle method componentDidMount to make call to api
+export class PostList extends Component {
+  render() {
+    return (
+      <div>
+        Post List
+      </div>
+    )
+  }
+}
+
+export default PostList;
