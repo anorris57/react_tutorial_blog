b683e46c09e98d4175a2ebb287704878560f16f2
Angela Norris <cocoanorris@yahoo.com>
2019-09-13 16:17:33 -0400

    using actioncreators inside actioncreators

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,12 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async dispatch => {
+  console.log('About to fetch posts');
+  await dispatch(fetchPosts());
+  console.log('fetched posts!');
+}
+
 //chosing not to remove the word return and leaving outer curly braces to help with clarity
 
 export const fetchPosts = () => {
@@ -11,19 +17,23 @@ export const fetchPosts = () => {
   };
 };
 
-/* export const fetchUser = (id) => {
+//following code isn't as simple as possible so that it is clearer to newbie what is going on
+
+export const fetchUser = (id) => {
   return async (dispatch) => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
 
     dispatch({ type:'FETCH_USER', payload: response.data });
   };
-}; */
+};
 
-export const fetchUser = id => dispatch => {
+//Below is code for using memoize to fix overfetching proble
+//There is a draw back that only 1 call per unique input
+/* export const fetchUser = id => dispatch => {
   _fetchUser(id, dispatch);
 };
 
 const _fetchUser = _.memoize(async (id, dispatch) => {
   const response = await jsonPlaceholder.get(`/users/${id}`);
   dispatch({ type: 'FETCH_USER', payload: response.data});
-});
+}); */
--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,11 +1,11 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostsAndUsers } from '../actions';
 import UserHeader from './UserHeader';
 //use life cycle method componentDidMount to make call to api
 export class PostList extends Component {
   componentDidMount() {
-    this.props.fetchPosts();
+    this.props.fetchPostsAndUsers();
   }
 
   renderList() {
@@ -37,4 +37,4 @@ const mapStateToProps = (state) => {
   return { posts: state.posts };
 }
 
-export default connect (mapStateToProps, {fetchPosts: fetchPosts})(PostList);
+export default connect (mapStateToProps, {fetchPostsAndUsers: fetchPostsAndUsers})(PostList);
