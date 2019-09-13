42dc9b0b8ccffdd3f3a4b0127c9939fb190d4969
Angela Norris <cocoanorris@yahoo.com>
2019-09-13 18:08:41 -0400

    finding actioncreator inside actioncreater with memoize

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,12 @@
 import _ from 'lodash';
 import jsonPlaceholder from '../apis/jsonPlaceholder';
 
-export const fetchPostsAndUsers = () => async dispatch => {
-  console.log('About to fetch posts');
+export const fetchPostsAndUsers = () => async (dispatch, getState) => {
+  //console.log('About to fetch posts');
   await dispatch(fetchPosts());
-  console.log('fetched posts!');
+
+  const userIds = _.uniq(_.map(getState().posts, 'userId'));
+  userIds.forEach(id => dispatch(fetchUser(id)));
 }
 
 //chosing not to remove the word return and leaving outer curly braces to help with clarity
--- blog/src/components/UserHeader.js
+++ blog/src/components/UserHeader.js
@@ -1,12 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchUser } from '../actions';
 
 class UserHeader extends React.Component { 
-   componentDidMount() {
-    this.props.fetchUser(this.props.userId);
-  }
-
   render() {
     //const user = this.props.users.find((user) => user.id === this.props.userId);
     const { user } = this.props;
@@ -24,5 +19,5 @@ const mapStateToProps = (state, ownProps) => {
   return { user: state.users.find(user => user.id === ownProps.userId) };
 }
 
-export default connect (mapStateToProps, {fetchUser: fetchUser})(UserHeader);
+export default connect (mapStateToProps)(UserHeader);
 
